7a7658ab8d3b4477255261cafaffe1ae61c75642
Edwin Sanchez <edwinsanchez750@gmail.com>
2021-09-19 18:21:00 -0500

    Added functionalities to verify the properties of a group and if its cyclic o abelian

--- projects/groups/src/Menu.js
+++ projects/groups/src/Menu.js
@@ -1,29 +1,50 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import store from './store'
-import { areInverses, isCommutative, findE } from './mathUtil'
+import { areInverses, isCommutative, findE, isAssociative, cyclic } from './mathUtil'
 import './assets/menu.css'
 
 export default function Menu() {
 
     const tableIsComplete = useSelector((state) => state.table.value.isComplete)
     let table = store.getState().table.value.elements
+
+    let inverses = false
+    let conmutativity = false
+    let modulativity = false
+    let asociativity = false
+    let isCyclic = false
+    let generator
+
     if (tableIsComplete) {
-        console.log(areInverses(table))
-        console.log(isCommutative(table))
-        console.log(findE(table))
+        inverses = areInverses(table)
+        conmutativity = isCommutative(table)
+        modulativity = findE(table) !== false
+        asociativity = isAssociative(table)
+        generator = cyclic(table)
+        isCyclic = generator !== false
     }
 
     return (
         <div id="menu">
             <h1>Grupo</h1>
             <h2>Propiedades</h2>
-            <p><input checked={isCommutative(table)} type="radio"></input> Asociatividad</p>
-            <p><input checked={findE(table)} type="radio"></input>Modulatividad</p>
-            <p><input checked={areInverses(table)} type="radio"></input>Inversos</p>
+            <p><input checked={asociativity}
+                readOnly
+                type="radio"></input> Asociatividad</p>
+            <p><input checked={modulativity}
+                readOnly
+                type="radio"></input>Modulatividad</p>
+            <p><input checked={inverses}
+                readOnly
+                type="radio"></input>Inversos</p>
             <h2>Otroas propiedades</h2>
-            <p><input checked={isCommutative(table)} type="radio"></input>Cíclico</p>
-            <p><input checked={isCommutative(table)} type="radio"></input>Abeliano</p>
+            <p><input checked={isCyclic}
+                readOnly
+                type="radio"></input>Cíclico {generator}</p>
+            <p><input checked={conmutativity}
+                readOnly
+                type="radio"></input>Abeliano</p>
             <h2>Subgrupos</h2>
         </div>
     )
--- projects/groups/src/mathUtil.js
+++ projects/groups/src/mathUtil.js
@@ -39,3 +39,60 @@ export function isCommutative(M){
 
 //let M=[[1,0,1],[0,1,2],[1,2,3]]
 //console.log(isCommutative(M))
+
+export function isAssociative(M){
+
+    let associative=true
+    if(M.length!==M[0].length){
+        throw (new Error("Matrix isn't square"))
+    } 
+        
+    for (let i=0; i<M.length;i++){
+        for( let j=0;j<M.length;j++){
+            for (let k=0;k<M.length;k++){
+                let right=M[i][M[j][k]]
+                let left=M[M[i][j]][k]
+                //console.log(`(${i}${j})${k},${right}`)
+                //console.log(`(${i}${j})${k},${left}`)
+                if(left!==right){
+                    associative = false
+                    
+                }
+            }
+        }
+    }
+    return associative
+}
+    
+//let Operation=[[0,1],[0,1]]
+//console.log(isAssociative(Operation))
+
+
+export function cyclic(M){
+
+    //falta distingir de generador a la izquierda de a la derecha
+    for (let i=0;i<M.length;i++){
+        
+        let results=[i]
+        let mayBeGenerator=true
+        
+        for(let j=0; j<M.length-1&&mayBeGenerator; j++){
+            
+            let n_power=M[results[results.length-1]][i]
+            
+            if(results.includes(n_power)){
+                mayBeGenerator=false
+            }
+            else{
+                results.push(M[results[results.length-1]][i])
+            }
+        }
+        
+        if(mayBeGenerator)return i
+    }
+    
+    return false
+}
+
+//let op=[[0,0],[0,1]]
+//console.log(cyclic(op))
